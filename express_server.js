c5e72ef9d413c3cecffa6bf41f7e3d7501bdaf0a
nicholasreimer <nchlsreimer@gmail.com>
2022-04-18 20:22:32 +0000

    new get request added to server code, returns the json info for the object urlDatabase

--- express_server.js
+++ express_server.js
@@ -6,14 +6,18 @@ const app = express();
 const PORT = 8080; // default port 8080
 
 //----------------------------------------------------
-
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
+//------------------------------------------------------
 
 app.get("/", (req, res) => {
   res.send("Hello!");
+});
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
 });
 
 //-------------------------------------------------------
