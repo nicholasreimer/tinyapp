fe8c0f9d6acb20fd6c5db5559b65af18b51d0752
nicholasreimer <nchlsreimer@gmail.com>
2022-04-20 23:17:21 +0000

    W3-D3 Registering New Users: a new registration page has been made that contains a form allowing clients to input email and password, on the server file these get attached to a new random string that serves as the id, the newly registered clients info get pushed to an user object that stores these values. In addition a cookie is made for the client with the id value so that the client is recognizable to the server wherever they travel through out the site.

--- express_server.js
+++ express_server.js
@@ -9,6 +9,7 @@ const bodyParser = require("body-parser");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
+
 //this code sets ejs as the template engine
 app.set("view engine", "ejs");
 
@@ -37,7 +38,7 @@ const users = {
 //----------------------------------------------------------------------------------------
 //GLOBAL FUNCTION: implement a function that returns a string of 6 random alphanumeric characters:
 function generateRandomString() {
-  return Math.random().toString(20).substr(2, 6);
+  return Math.random().toString(20).substring(2, 6);
 }
 //code source: https://dev.to/oyetoket/fastest-way-to-generate-random-strings-in-javascript-2k5a
 
@@ -46,17 +47,22 @@ function generateRandomString() {
 
 // -GET route request renders the urls_new.ejs template in the browser
 app.get("/urls/new", (req, res) => {
-  const templateVars = { username: req.cookies["username"] };
+  const templateVars = {
+    username: users[req.cookies["user_id"]],
+  };
   res.render("urls_new", templateVars);
 });
 
 //post route request gives clients a random string in place of ther inputed long url
 app.post("/urls", (req, res) => {
-  // -assign a random string to a var called shortURL and add shortURL as a new key inside
-  //  urlDatabase object with the user input for longurl as its key value.
   let shortURL = generateRandomString();
-  urlDatabase[shortURL] = req.body.longURL;
+  let longURL = req.body.longURL;
 
+  // in the case of a missing http input for longURL this code adds it to the clients input to make it whole.
+  if (!longURL.includes("http")) {
+    longURL = "http://" + longURL;
+  }
+  urlDatabase[shortURL] = longURL;
   res.redirect(`/urls/${shortURL}`); //redirect the client to the shortUrl page specific to there new shortURL
 });
 
@@ -65,13 +71,19 @@ app.post("/urls", (req, res) => {
 app.get("/u/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL;
   const longURL = urlDatabase[shortURL];
+  //*** GET /u/:id - https://web.compass.lighthouselabs.ca/projects/tiny-app (implement: conditional that says if(not long url)
+  if (!longURL) {
+    return res.send("The given short-URL does not exsist");
+  }
   res.redirect(longURL);
 });
 
 //get route request that renders a page of all the urls stored currently in urlDatabase
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase, username: req.cookies["username"] };
-
+  const templateVars = {
+    urls: urlDatabase,
+    username: users[req.cookies["user_id"]],
+  };
   res.render("urls_index", templateVars);
 });
 
@@ -87,7 +99,13 @@ app.post("/urls/:shortURL/delete", (req, res) => {
 //  by accesssing its shortURL key and refreshes the page via a redirect.
 app.post("/urls/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL;
-  urlDatabase[shortURL] = req.body.updatedURL;
+  let longURL = req.body.updatedURL;
+
+  // in the case of a missing http input for longURL this code adds it to the clients input to make it whole.
+  if (!longURL.includes("http")) {
+    longURL = "http://" + longURL;
+  }
+  urlDatabase[shortURL] = longURL;
   res.redirect("/urls");
 });
 
@@ -100,7 +118,7 @@ app.get("/urls/:shortURL", (req, res) => {
   const templateVars = {
     shortURL: shortURL,
     longURL: urlDatabase[shortURL],
-    username: req.cookies["username"],
+    username: users[req.cookies["user_id"]],
   };
 
   res.render("urls_show", templateVars);
@@ -111,13 +129,13 @@ app.get("/urls/:shortURL", (req, res) => {
 
 // post route request makes it possible for users to create a username and connects a cookie to them
 app.post("/login", (req, res) => {
-  res.cookie("username", req.body.username);
+  res.cookie("user_id", 1);
   res.redirect("/urls");
 });
 
 //post route request that allows a use to logout, redirects them to the register page
 app.post("/logout", (req, res) => {
-  res.clearCookie("username");
+  res.clearCookie("user_id");
   res.redirect("/register");
 });
 
@@ -125,14 +143,27 @@ app.post("/logout", (req, res) => {
 //get route request that allows users to create a username and password
 app.get("/register", (req, res) => {
   const templateVars = {
-    username: req.cookies["username"],
+    username: users[req.cookies["user_id"]],
   };
 
   res.render("register", templateVars);
 });
 
-// the POST endpoint for login
-app.post("/register", (req, res) => {});
+// the POST endpoint for register
+app.post("/register", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  const id = generateRandomString();
+
+  users[id] = {
+    id,
+    email,
+    password,
+  };
+
+  res.cookie("user_id", id);
+  res.redirect("/urls");
+});
 
 //----------------------------------------------------------------------------------------
 //CLIENT REQUEST LISTENER:
